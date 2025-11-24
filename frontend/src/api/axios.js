9b46283b174735bf5b3041139bd752580570747c
ex <ex@mini4.lan>
2025-11-24 22:21:30 +0800

    Feat: add CORS problem 3

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8003', // Using port 8003 as verified
+    baseURL: '/api',
     headers: {
         'Content-Type': 'application/json'
     }
