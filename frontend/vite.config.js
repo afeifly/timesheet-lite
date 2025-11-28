02ab67ef45919c4b8791db99107a7b49513143dd
afeifly <afeidreamfly@gmail.com>
2025-11-28 02:57:00 +0000

    Run frontend with proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     plugins: [vue()],
     server: {
         host: '0.0.0.0', // Listen on all network interfaces
+	allowedHosts: ['test.exmm.top','eco.exmm.top'],
         proxy: {
             '/api': {
                 target: 'http://127.0.0.1:8003',
